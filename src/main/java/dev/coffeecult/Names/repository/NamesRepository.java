6fd7fa20ceac2b04cd77082834abfcebba31e33c
SoyNegro <krisoul.valdes@gmail.com>
2023-04-23 14:49:20 -0400

    Name Repository added

---
+++ src/main/java/dev/coffeecult/Names/repository/NamesRepository.java
@@ -0,0 +1,10 @@
+package dev.coffeecult.Names.repository;
+
+import dev.coffeecult.Names.model.Name;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NamesRepository extends JpaRepository<Name,Long> {
+    boolean existsByFirstName(String firstName);
+}
