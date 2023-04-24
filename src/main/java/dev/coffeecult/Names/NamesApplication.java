cc630cba29b6ad4d697b3c221fb65f00f8547709
SoyNegro <krisoul.valdes@gmail.com>
2023-04-23 22:50:23 -0400

    Deleted test values

--- src/main/java/dev/coffeecult/Names/NamesApplication.java
+++ src/main/java/dev/coffeecult/Names/NamesApplication.java
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NamesApplication {
-  @Value("${spring.datasource.password}")
-  private static String pass;
 	public static void main(String[] args) {
 		SpringApplication.run(NamesApplication.class, args);
-		System.out.println(pass);
 	}
 
 }
